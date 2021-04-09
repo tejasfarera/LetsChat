b230f33fd9fb9760febce7584bd84294da5f3c39
macmini45 <mac@Chayan.local>
2021-04-09 13:04:32 +0530

    Initial setup

---
+++ LetsChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LetsChat
+//
+//  Created by macmini45 on 09/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LetsChat/LetsChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsChatApp.swift
+//  LetsChat
+//
+//  Created by macmini45 on 09/04/21.
+//
+
+import SwiftUI
+
+@main
+struct LetsChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
